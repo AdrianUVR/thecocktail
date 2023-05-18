8c2a379bf13864d599a0fb6228fc05974a1965d7
PLauraCruz <plauracruz03@gmail.com>
2023-05-18 12:52:44 -0600

    Add project files.

---
+++ thecocktails/Controllers/TheCocktailController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+
+namespace thecocktails.Controllers
+{
+    public class TheCocktailController : Controller
+    {
+        public ActionResult Drinks()
+        {
+            Models.TheCocktail thecocktail = new Models.TheCocktail();
+            using (var client = new HttpClient())
+            {
+
+                client.BaseAddress = new Uri("http://www.thecocktaildb.com/api/json/v1/1/");
+                var resdponsetask = client.GetAsync("search.php?s=");
+                resdponsetask.Wait();
+                var result = resdponsetask.Result;
+
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsStringAsync();
+                    dynamic resultJSON = JObject.Parse(readTask.Result.ToString());
+                    readTask.Wait();
+                    thecocktail.TheCocktails = new List<object>();
+                    foreach (var resultitem in resultJSON.drinks)
+                    {
+                        Models.TheCocktail cocktailitem = new Models.TheCocktail();
+                        cocktailitem.idDrink = resultitem.idDrink;
+                        cocktailitem.Instrucciones = resultitem.strInstructionsDE;
+                        cocktailitem.Nombre = resultitem.strDrink;
+                        cocktailitem.Imagen = resultitem.strDrinkThumb;
+                        thecocktail.TheCocktails.Add(cocktailitem);
+                    }
+
+                }
+
+            }
+            return View(thecocktail);
+        }
+        [HttpPost]
+        public ActionResult Drinks(Models.TheCocktail thecocktail)
+        {
+         
+            using (var client = new HttpClient())
+            {
+
+                client.BaseAddress = new Uri("http://www.thecocktaildb.com/api/json/v1/1/");
+                var resdponsetask = client.GetAsync("search.php?s="+ thecocktail.busqueda);
+                resdponsetask.Wait();
+                var result = resdponsetask.Result;
+
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsStringAsync();
+                    dynamic resultJSON = JObject.Parse(readTask.Result.ToString());
+                    readTask.Wait();
+                    thecocktail.TheCocktails = new List<object>();
+                    foreach (var resultitem in resultJSON.drinks)
+                    {
+                        Models.TheCocktail cocktailitem = new Models.TheCocktail();
+                        cocktailitem.idDrink = resultitem.idDrink;
+                        cocktailitem.Instrucciones = resultitem.strInstructionsDE;
+                        cocktailitem.Nombre = resultitem.strDrink;
+                        cocktailitem.Imagen = resultitem.strDrinkThumb;
+                        thecocktail.TheCocktails.Add(cocktailitem);
+                    }
+
+                }
+
+            }
+            return View(thecocktail);
+        }
+    }
+}
---
+++ thecocktails/Models/TheCocktail.cs
@@ -0,0 +1,26 @@
+﻿namespace thecocktails.Models
+{
+    public class TheCocktail
+    {
+
+        public int idDrink { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Categoria { get; set; }
+
+        public string strAlcoholic { get; set; }
+
+        public string Instrucciones { get; set; }
+
+
+        public string Ingredientes { get; set; }
+
+        public string Imagen { get; set; }
+
+        public string busqueda { get; set; }
+
+
+        public List<object> TheCocktails { get; set; }
+    }
+}
